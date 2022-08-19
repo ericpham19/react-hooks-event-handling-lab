0ab7a8b83410b46f63dbac09abb3de5f414518aa
Duy Pham <ericpham19@outlook.com>
2022-08-19 13:40:53 +1000

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+function Keypad(){
+    function handleChange(){
+        console.log("Entering password...")
+    }
+    return (
+    <input 
+        type = "password"
+        name = "search"
+        onChange = {handleChange}
+        placeholder = "Enter password"
+    
+    />
+
+    )
+}
+
+export default Keypad;
